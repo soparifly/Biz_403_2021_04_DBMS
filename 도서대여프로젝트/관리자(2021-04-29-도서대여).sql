87ab9b4096930fb782da812deee602c7af4767b3
soparifly <soparifly@naver.com>
2021-05-01 19:31:27 +0900

    2021-04-30 home에서 한것

---
+++ 도서대여프로젝트/관리자(2021-04-29-도서대여).sql
@@ -0,0 +1,11 @@
+--여기는 관리자 접속
+--프로젝트에서 사용할 TableSpace, User생성
+
+CREATE TABLESPACE RentBookDB
+DATAFILE 'C:/oraclexe/data/rentbook.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 1K;
+
+CREATE USER bookuser IDENTIFIED BY bookuser
+DEFAULT TABLESPACE RentBookDB;
+
+GRANT DBA TO bookuser;
